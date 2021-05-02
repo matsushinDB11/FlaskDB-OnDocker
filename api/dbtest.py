e9d2461575fd5670188ccf0d9e5fd3bfac01c5c7
Shintaro Matsumoto <matsushin35@gmail.com>
2021-05-02 14:05:35 +0900

    add db_test

--- api/dbtest.py
+++ api/dbtest.py
@@ -1,35 +1,29 @@
 import MySQLdb
 from MySQLdb import connections
 
-
 con = MySQLdb.connect(
-        user='apiuser',
-        passwd='apipasswd',
-        host='db',
-        db='sample01',
-        charset='utf8'
-    )
+    user='apiuser',
+    passwd='apipasswd',
+    host='db',
+    db='sample01',
+    charset='utf8'
+)
 cur = con.cursor()
 
 
 def db_items_insert():
-    sql = "INSERT INTO items VALUES (%s, %s)"
+    sql = "INSERT INTO items (name) VALUES (%s)"
     # sql1 = "INSERT INTO items VALUES (1, 'Mercedes')"
     # sql2 = "INSERT INTO items VALUES (2, 'RedBull')"
 
-    # cur.execute(sql, (1, 'Mercedes'))
-    # cur.execute(sql, (2, "Redbull"))
-    data = [
-        (1, 'Mercedes'),
-        (2, 'RedBull'),
-        (3, 'Ferrari')
-    ]
+    # cur.execute(sql, ('Mercedes'))
+    # cur.execute(sql, ("Redbull"))
+    data = ['Mercedes', 'RedBull', 'Ferrari']
     cur.executemany(sql, data)
     con.commit()
 
 
 def db_items_get():
-
     # クエリを実行する
     sql = "SELECT * FROM items"
     cur.execute(sql)
